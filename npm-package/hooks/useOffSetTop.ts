fc6f3a052d9bf3cde9aa58bfbcf3dd101913c339
Deep Vadukiya <dipvadukiya06@gmail.com>
2023-08-29 21:30:31 +0530

    useOffSetTop - applied intellisense to the new hook

--- npm-package/hooks/useOffSetTop.ts
+++ npm-package/hooks/useOffSetTop.ts
@@ -2,6 +2,16 @@
 
 import { useState, useEffect } from "react";
 
+/**
+ * this hook can be use to check if the page has scrolled desired pixels of not.
+ *
+ * ⚠️ Note: make sure if you don't pass any values to hook, it will consider 100 as a default value.
+ *
+ * @param top the number of pixels from top. if the page has scrolled that much pixels the hook will return true or else false
+ *
+ * @returns it will return boolean value if browser is scrolled that much pixels or not.
+ */
+
 export default function useOffSetTop(top: number): boolean {
   const [offsetTop, setOffSetTop] = useState(false);
   const isTop = top || 100;
