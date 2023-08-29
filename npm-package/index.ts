8a96da9ed877d45b068e768d1bc9ba7370140b57
Deep Vadukiya <dipvadukiya06@gmail.com>
2023-08-29 21:29:46 +0530

    useOffSetTop - added new hook named useOffSetTop in typescript

--- npm-package/index.ts
+++ npm-package/index.ts
@@ -3,5 +3,6 @@
 import useLocalStorage from "./hooks/localStorage";
 import useOnlineStatus from "./hooks/onlineStatus";
 import useHover from "./hooks/useHover";
+import useOffSetTop from "./hooks/useOffSetTop";
 
-export { useLocalStorage, useOnlineStatus, useHover };
+export { useLocalStorage, useOnlineStatus, useHover, useOffSetTop };
