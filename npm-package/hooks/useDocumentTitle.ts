99648e9064da27fd1590d12c079d7abfa6fdf569
Deep Vadukiya <dipvadukiya06@gmail.com>
2023-08-30 15:33:45 +0530

    useDocumentTitle - added new hook

---
+++ npm-package/hooks/useDocumentTitle.ts
@@ -0,0 +1,9 @@
+//
+
+import { useEffect } from "react";
+
+export default function useDocumentTitle(title: string): void {
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+}
--- npm-package/index.ts
+++ npm-package/index.ts
@@ -5,6 +5,7 @@ import useOnlineStatus from "./hooks/onlineStatus";
 import useHover from "./hooks/useHover";
 import useOffSetTop from "./hooks/useOffSetTop";
 import useKeyPress from "./hooks/useKeyPress";
+import useDocumentTitle from "./hooks/useDocumentTitle";
 
 export {
   useLocalStorage,
@@ -12,4 +13,5 @@ export {
   useHover,
   useOffSetTop,
   useKeyPress,
+  useDocumentTitle,
 };
