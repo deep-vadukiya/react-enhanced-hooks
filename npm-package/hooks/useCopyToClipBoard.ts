74a5bf1f074eb9618faa62a4b0898008167c2a8f
Deep Vadukiya <dipvadukiya06@gmail.com>
2023-08-31 08:59:27 +0530

    useCopyToClipBoard | applied intellisense to the useCopyToClipboard hook

--- npm-package/hooks/useCopyToClipBoard.ts
+++ npm-package/hooks/useCopyToClipBoard.ts
@@ -2,6 +2,17 @@
 
 import { useState } from "react";
 
+/**
+ *
+ * This hook can be used to copy the any string value to users clipboard.
+ *
+ * isCopied(boolean): returns if the value is copied to clipboard.
+ *
+ * copyToClipboard(method): takes the string value and copies it to clipboard.
+ *
+ * @returns returns the object with the properties of copy status and function which can be used to pass the string values which need to be copy.
+ */
+
 export default function useCopyToClipBoard(): any {
   const [isCopied, setIsCopied] = useState(false);
 
