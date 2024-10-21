cc3fa87bb7ac4dd0d19ca8bbcb095c2fc7373362
Deep Vadukiya <dipvadukiya06@gmail.com>
2024-10-21 16:09:26 +0530

    FE06 - GitHub Pages - respective build and deployment updates on next.config

--- next.config.js
+++ next.config.js
@@ -3,16 +3,19 @@
 // next.config.js
 import nextra from "nextra";
 
+
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true // mandatory, otherwise won't export
+  }
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: "build"
+}
+
 const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.js",
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 });
 
-export default withNextra();
+export default withNextra(nextConfig);
