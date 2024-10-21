113f8fe93a653fdda54c91e387d59fd3b2b16c62
Deep Vadukiya <dipvadukiya06@gmail.com>
2024-10-21 22:26:07 +0530

    removed nextConfig option

--- next.config.js
+++ next.config.js
@@ -4,16 +4,16 @@
 import nextra from "nextra";
 
 
-const nextConfig = {
-  // images: {
-  //   unoptimized: true // mandatory, otherwise won't export
-  // },
-  distDir: 'out',
-}
+// const nextConfig = {
+// images: {
+//   unoptimized: true // mandatory, otherwise won't export
+// },
+// distDir: 'out',
+// }
 
 const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.js",
 });
 
-export default withNextra(nextConfig);
+export default withNextra();
