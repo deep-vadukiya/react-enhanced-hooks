74f8d9d9c49f7df799bb4fbecec6938b8b5d4268
Deep Vadukiya <dipvadukiya06@gmail.com>
2024-10-22 15:13:53 +0530

    FE07 - Landing page: added tailwind to the app and basic landing page components

--- components/index-page/index.jsx
+++ components/index-page/index.jsx
@@ -1,9 +1,11 @@
 //
 
+import { Hero } from "./hero";
+
 export function IndexPage() {
   return (
     <div className="graph-background h-dvh" style={{ height: "100vh" }}>
-      <div className="flex-auto justify-center"></div>
+      <Hero />
     </div>
   );
 }
