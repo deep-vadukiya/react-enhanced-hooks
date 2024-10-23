193ef78f18586b11918d2d026c525526eb9eac27
Deep Vadukiya <dipvadukiya06@gmail.com>
2024-10-23 13:41:35 +0530

    FE09-feat-documentation: added documentation for useOffsetTop hook

--- pages/documentation/hooks/_meta.js
+++ pages/documentation/hooks/_meta.js
@@ -4,5 +4,6 @@ export default {
     ['use-local-storage']: 'useLocalStorage',
     ['use-online-status']: 'useOnlineStatus',
     ['use-hover']: 'useHover',
+    ['use-offset-top']: 'useOffSetTop',
     ['use-key-combination']: 'useKeyCombination',
 }
