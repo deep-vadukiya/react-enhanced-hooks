417fa19984931feae7ee9888ed9fdd5a285611eb
Deep Vadukiya <dipvadukiya06@gmail.com>
2024-10-23 14:27:51 +0530

    fix: nextra documentation - fixed dynamic title and content

--- theme.config.js
+++ theme.config.js
@@ -1,15 +1,10 @@
 //
 
+import { useConfig } from 'nextra-theme-docs'
+
 // theme.config.js
 export default {
   logo: <b>React Enhanced Hooks</b>,
-  head: (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="React Enhanced Hooks" />
-      <meta property="og:description" content="A custom useful hooks for your react project" />
-    </>
-  ),
   project: {
     link: "https://github.com/deep-vadukiya/react-enhanced-hooks",
   },
@@ -20,4 +15,17 @@ export default {
   footer: {
     component: false
   },
+  head() {
+    const { frontMatter } = useConfig();
+
+    return (
+      <>
+        <meta property="og:title" content={frontMatter.title || 'React Enhanced Hooks'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'A collection of useful custom hooks for React web applications.'}
+        />
+      </>
+    )
+  }
 };
