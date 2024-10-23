cbc9349aff4e77091b61270579c8b350cace380b
Deep Vadukiya <dipvadukiya06@gmail.com>
2024-10-23 15:26:36 +0530

    nextra site: added custom footer with links and platforms

---
+++ globalLinks.js
@@ -0,0 +1,16 @@
+//
+
+// platforms
+export const VERCEL_PLATFORM_LINK = "https://nextjs.org";
+export const GITHUB_PLATFORM_LINK = "https://github.com";
+
+// profiles
+export const GITHUB_PROFILE_LINK = "https://github.com/deep-vadukiya";
+export const GITHUB_REPOSITORY_LINK =
+  "https://github.com/deep-vadukiya/react-enhanced-hooks";
+
+// hostings
+export const GITHUB_PAGES_LINK =
+  "https://deep-vadukiya.github.io/react-enhanced-hooks";
+export const NPM_PACKAGEREGISTRY_LINK =
+  "https://www.npmjs.com/package/react-enhanced-hooks";
